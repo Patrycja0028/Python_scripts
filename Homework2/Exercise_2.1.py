6fd0935372984ef8649bbe0f199acd2ec39047dd
Patrycja0028 <patrycja.sarga@doctoral.uj.edu.pl>
2026-03-15 12:25:11 +0100

    Create Exercise_2.1.py

---
+++ Homework2/Exercise_2.1.py
@@ -0,0 +1,8 @@
+# Create a long positive integer. Find the number of zeros. Hint: change the number to a string. 
+
+n = 1940402000481000410
+s = str(n)
+zeros = s.count("0")
+zeros
+
+#print(zeros)
