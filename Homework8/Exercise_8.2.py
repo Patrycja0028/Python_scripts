0c9088a026bfd445e80a34f6dc67349f95620e17
Patrycja0028 <patrycja.sarga@doctoral.uj.edu.pl>
2026-05-01 17:21:01 +0200

    Update Exercise_8.2.py

--- Homework8/Exercise_8.2.py
+++ Homework8/Exercise_8.2.py
@@ -1,11 +1,11 @@
 # (a) Create an arbitrary one dimensional array called v1.
 v1 = np.array([10, 20, 30, 40, 50, 60, 70, 80, 90])
-print("8.2 (a) v1:", v1)
+print("(a) v1:", v1)
 
 # (b) Create a new array (or a view) v2 which consists of items with odd indices of v1.
 v2 = v1[1::2]
-print("8.2 (b) v2 (odd indices):", v2)
+print("(b) v2 (odd indices):", v2)
 
 # (c) Create a new array (or a view) v3 in backwards ordering from v1. 
 v3 = v1[::-1]
-print("8.2 (c) v3 (reversed):", v3)
+print("(c) v3 (reversed):", v3)
